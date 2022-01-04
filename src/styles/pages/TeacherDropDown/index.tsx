5eee893e25421367bc66af87cdb19acdb87634d4
EvertonAdame <87421330+EvertonAdame@users.noreply.github.com>
2022-01-04 08:22:01 -0300

    teste

--- src/styles/pages/TeacherDropDown/index.tsx
+++ src/styles/pages/TeacherDropDown/index.tsx
@@ -6,7 +6,7 @@ import { Container, StyledButton } from './styles';
 
 export const DropdownMenu = ({
   isLoading = false,
-}: DropdownMenuProps): JSX.Element => {
+}): JSX.Element => {
 
 const [open, setOpen] = useState(false);
 
@@ -14,7 +14,7 @@ const [open, setOpen] = useState(false);
     <Container>
       <FaEllipsisV onClick={() => setOpen((state) => !state)} size={22} />
       <ul className={`options ${open ? 'menu-visible' : ''}`}>
-        <StyledButton type="button" className="student-doubt" disabled={isLoading}>
+        <StyledButton type="button" className="student-doubt">
           <li>{isLoading ? 'Carregando...' : ''}</li>
         </StyledButton>
       </ul>
